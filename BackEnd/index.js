d42b86733a69f29c8576514dea3415d5459c9c6e
venkataprabhath15 <prabhaththatavarti@gmail.com>
2026-03-09 00:33:24 +0530

    origin url fix

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 connectDB();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://music-player-app-black.vercel.app/"],
     credentials: true,
 }))
 
